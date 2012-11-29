c6444eca2562a1311e68fe6ced48ee6731140608
Ruben Verborgh <ruben@verborgh.org>
2012-11-29 17:11:09 +0100

    Move service JSON writing and reading code to service manager.

--- src/org/freeyourmetadata/ner/commands/ServicesCommand.java
+++ src/org/freeyourmetadata/ner/commands/ServicesCommand.java
@@ -1,16 +1,10 @@
 package org.freeyourmetadata.ner.commands;
 
-import java.util.Iterator;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.freeyourmetadata.ner.services.NERService;
 import org.freeyourmetadata.ner.services.NERServiceManager;
 import org.json.JSONArray;
-import org.json.JSONObject;
 import org.json.JSONWriter;
-
-import com.google.refine.util.JSONUtilities;
 
 public class ServicesCommand extends NERCommand {
     private final NERServiceManager serviceManager;
@@ -21,45 +15,13 @@ public class ServicesCommand extends NERCommand {
     
     @Override
     public void get(HttpServletRequest request, JSONWriter response) throws Exception {
-        /* Array of services */
-        response.array();
-        for(NERService service : serviceManager.getServices()) {
-            /* Service object */
-            response.object();
-            response.key("name");
-            response.value(service.getName());
-            
-            /* Service settings object */
-            response.key("settings");
-            response.object();
-            for(String propertyName : service.getPropertyNames()) {
-                response.key(propertyName);
-                response.value(service.getProperty(propertyName));
-            }
-            response.endObject();
-            
-            response.endObject();
-        }
-        response.endArray();
+        serviceManager.writeTo(response);
     }
     
     @Override
-    @SuppressWarnings("unchecked")
     public void put(HttpServletRequest request, Object body, JSONWriter response) throws Exception {
         if(!(body instanceof JSONArray))
             throw new IllegalArgumentException("Body should be a JSON array.");
-        final Object[] services = JSONUtilities.toArray((JSONArray)body);
-        for(Object value : services) {
-            if(!(value instanceof JSONObject))
-                throw new IllegalArgumentException("Body should be an array of JSON objects.");
-            final JSONObject serviceValue = (JSONObject)value;
-            final NERService service = serviceManager.getService(serviceValue.getString("name"));
-            final JSONObject settings = serviceValue.getJSONObject("settings");
-            final Iterator<String> settingNames = settings.keys();
-            while(settingNames.hasNext()) {
-                final String settingName = settingNames.next();
-                service.setProperty(settingName, settings.getString(settingName));
-            }
-        }
+        serviceManager.updateFrom((JSONArray)body);
     }
 }
--- src/org/freeyourmetadata/ner/services/NERServiceManager.java
+++ src/org/freeyourmetadata/ner/services/NERServiceManager.java
@@ -1,6 +1,14 @@
 package org.freeyourmetadata.ner.services;
 
 import java.util.HashMap;
+import java.util.Iterator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONWriter;
+
+import com.google.refine.util.JSONUtilities;
 
 public class NERServiceManager {
     private final HashMap<String, NERService> services;
@@ -21,5 +29,45 @@ public class NERServiceManager {
     
     public NERService[] getServices() {
         return services.values().toArray(new NERService[services.size()]);
+    }
+    
+    public void writeTo(JSONWriter output) throws JSONException {
+        /* Array of services */
+        output.array();
+        for(NERService service : getServices()) {
+            /* Service object */
+            output.object();
+            output.key("name");
+            output.value(service.getName());
+            
+            /* Service settings object */
+            output.key("settings");
+            output.object();
+            for(String propertyName : service.getPropertyNames()) {
+                output.key(propertyName);
+                output.value(service.getProperty(propertyName));
+            }
+            output.endObject();
+            
+            output.endObject();
+        }
+        output.endArray();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public void updateFrom(JSONArray serviceValues) throws JSONException {
+        final Object[] services = JSONUtilities.toArray((JSONArray)serviceValues);
+        for(Object value : services) {
+            if(!(value instanceof JSONObject))
+                throw new IllegalArgumentException("Value should be an array of JSON objects.");
+            final JSONObject serviceValue = (JSONObject)value;
+            final NERService service = getService(serviceValue.getString("name"));
+            final JSONObject settings = serviceValue.getJSONObject("settings");
+            final Iterator<String> settingNames = settings.keys();
+            while(settingNames.hasNext()) {
+                final String settingName = settingNames.next();
+                service.setProperty(settingName, settings.getString(settingName));
+            }
+        }
     }
 }
