bf178bf0a1455734d8e773e1bb92722fd1da611e
Ruben Verborgh <ruben@verborgh.org>
2012-11-29 20:01:02 +0100

    Create service from class name if it doesn't exist.

--- src/org/freeyourmetadata/ner/services/NERServiceManager.java
+++ src/org/freeyourmetadata/ner/services/NERServiceManager.java
@@ -19,11 +19,9 @@ public class NERServiceManager {
     private final HashMap<String, NERService> services;
     private final File settingsFile;
     
-    public NERServiceManager(File settingsFile) throws IOException, JSONException {
+    public NERServiceManager(final File settingsFile) throws IOException, JSONException, ClassNotFoundException {
         this.settingsFile = settingsFile;
         services = new HashMap<String, NERService>();
-        addService(new Zemanta());
-        addService(new DummyNER());
         
         if (settingsFile.exists()) {
             JSONTokener tokener = new JSONTokener(new FileReader(settingsFile));
@@ -31,14 +29,35 @@ public class NERServiceManager {
         }
     }
     
-    public void addService(NERService service) {
+    public boolean hasService(final String serviceName) {
+        return services.containsKey(serviceName);
+    }
+    
+    public void addService(final NERService service) {
         services.put(service.getName(), service);
     }
     
-    public NERService getService(String name) {
+    public NERService getService(final String name) {
         if (!services.containsKey(name))
             throw new IllegalArgumentException("No service named " + name + " exists.");
         return services.get(name);
+    }
+    
+    protected NERService getOrCreateService(final String serviceName, final String className) throws ClassNotFoundException {
+        final NERService service;
+        if (hasService(serviceName)) {
+            service = getService(serviceName);
+        }
+        else {
+            final Class<?> serviceClass = getClass().getClassLoader().loadClass(className);
+            try {
+                service = (NERService)serviceClass.newInstance();
+            }
+            catch (InstantiationException error) { throw new RuntimeException(error); }
+            catch (IllegalAccessException error) { throw new RuntimeException(error); }
+            addService(service);
+        }
+        return service;
     }
     
     public NERService[] getServices() {
@@ -51,14 +70,16 @@ public class NERServiceManager {
         writer.close();
     }
     
-    public void writeTo(JSONWriter output) throws JSONException {
+    public void writeTo(final JSONWriter output) throws JSONException {
         /* Array of services */
         output.array();
-        for(NERService service : getServices()) {
+        for (NERService service : getServices()) {
             /* Service object */
             output.object();
             output.key("name");
             output.value(service.getName());
+            output.key("class");
+            output.value(service.getClass().getName());
             
             /* Service settings object */
             output.key("settings");
@@ -75,16 +96,16 @@ public class NERServiceManager {
     }
     
     @SuppressWarnings("unchecked")
-    public void updateFrom(JSONArray serviceValues) throws JSONException {
+    public void updateFrom(final JSONArray serviceValues) throws JSONException, ClassNotFoundException {
         final Object[] services = JSONUtilities.toArray((JSONArray)serviceValues);
-        for(Object value : services) {
-            if(!(value instanceof JSONObject))
+        for (Object value : services) {
+            if (!(value instanceof JSONObject))
                 throw new IllegalArgumentException("Value should be an array of JSON objects.");
             final JSONObject serviceValue = (JSONObject)value;
-            final NERService service = getService(serviceValue.getString("name"));
+            final NERService service = getOrCreateService(serviceValue.getString("name"), serviceValue.getString("class"));
             final JSONObject settings = serviceValue.getJSONObject("settings");
             final Iterator<String> settingNames = settings.keys();
-            while(settingNames.hasNext()) {
+            while (settingNames.hasNext()) {
                 final String settingName = settingNames.next();
                 service.setProperty(settingName, settings.getString(settingName));
             }
