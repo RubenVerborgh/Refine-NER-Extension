9aff04b873bc1c267f7ee5dcc05608f7d716893c
Ruben Verborgh <ruben@verborgh.org>
2012-11-30 11:16:03 +0100

    Add service checkboxes to extraction dialog.

--- module/dialogs/extraction.js
+++ module/dialogs/extraction.js
@@ -3,20 +3,40 @@ function ExtractionDialog(column) {
 }
 
 ExtractionDialog.prototype = {
-  init: function () {
-    var dialogElement = this.dialogElement = $(DOM.loadHTML("named-entity-recognition", "dialogs/extraction.html"));
+  init: function (callback) {
+    var self = this,
+        selectedServices = {},
+        dialogElement = this.dialogElement = $(DOM.loadHTML("named-entity-recognition", "dialogs/extraction.html"));
     
     /* Set labels */
     $('.column-name', dialogElement).text(this.column.name);
     
-    /* Bind controls */
+    /* Bind controls to actions */
     var controls = DOM.bind(this.dialogElement);
     controls.cancel.click(this.bound("hide"));
+    
+    /* Load service checkboxes */
+    $.getJSON(NERExtension.servicesPath, function (services) {
+      var $services = $(controls.services);
+      services.forEach(function (service) {
+        var $service = $('<input/>', {
+                          type: 'checkbox',
+                          change: function (event) {
+                            selectedServices[service.name] = $(event.target).is(':checked');
+                          }}),
+            $label = $('<label/>', { 'class': 'checkbox' })
+                        .append($service, service.name);
+        $services.append($('<li/>').append($label));
+      });
+      if (callback)
+        callback.apply(self);
+    });
   },
   
   show: function () {
-    this.init();
-    this.dialogLevel = DialogSystem.showDialog(this.dialogElement);
+    this.init(function () {
+      this.dialogLevel = DialogSystem.showDialog(this.dialogElement);
+    });
   },
   
   hide: function () {
