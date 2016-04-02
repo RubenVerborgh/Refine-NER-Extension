RubenVerborgh/Refine-NER-Extension

commit 09def59d005d817fc84bbdf8319f1647064033af

Show DataTXT service errors in cells.
Part of #8.

src/org/freeyourmetadata/ner/operations/NERProcess.java
@@ -6,8 +6,8 @@ import java.util.Map;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
+import org.freeyourmetadata.ner.services.ExtractionResult;
 import org.freeyourmetadata.ner.services.NERService;
-import org.freeyourmetadata.ner.services.NamedEntity;
 import org.json.JSONObject;
 
 import com.google.refine.browsing.Engine;
@@ -27,8 +27,7 @@ import com.google.refine.process.LongRunningProcess;
  */
 public class NERProcess extends LongRunningProcess implements Runnable {
     private final static Logger LOGGER = Logger.getLogger(NERProcess.class);
-    private final static NamedEntity[][] EMPTY_RESULT_SET = new NamedEntity[0][];
-    
+
     private final Project project;
     private final Column column;
     private final Map<String, NERService> services;
@@ -66,7 +65,7 @@ public class NERProcess extends LongRunningProcess implements Runnable {
     public void run() {
         final int columnIndex = project.columnModel.getColumnIndexByName(column.getName()) + 1;
         final String[] serviceNames = services.keySet().toArray(new String[services.size()]);
-        final NamedEntity[][][] namedEntities = performExtraction();
+        final ExtractionResult[][] namedEntities = performExtraction();
         
         if (!_canceled) {
             project.history.addEntry(new HistoryEntry(historyEntryId, project, _description, parentOperation,
@@ -79,7 +78,7 @@ public class NERProcess extends LongRunningProcess implements Runnable {
      * Performs named-entity extraction on all rows
      * @return The extracted named entities per row and service
      */
-    protected NamedEntity[][][] performExtraction() {
+    protected ExtractionResult[][] performExtraction() {
         // Count all rows
         final int rowsTotal = project.rows.size();
         // Get the cell index of the column in which to perform entity extraction
@@ -89,11 +88,9 @@ public class NERProcess extends LongRunningProcess implements Runnable {
         final int rowsFiltered = filteredRowIndices.size();
         
         // Go through each row and extract entities if the row is part of the filter
-        final NamedEntity[][][] namedEntities = new NamedEntity[rowsTotal][][];
+        final ExtractionResult[][] extractionResults = new ExtractionResult[rowsTotal][];
         int rowsProcessed = 0;
         for (int rowIndex = 0; rowIndex < rowsTotal; rowIndex++) {
-            // Initialize to the empty result set, in case no entities are extracted
-            namedEntities[rowIndex] = EMPTY_RESULT_SET;
             // If the row is part of the filter, extract entities
             if (filteredRowIndices.contains(rowIndex)) {
                 final Row row = project.rows.get(rowIndex);
@@ -101,19 +98,19 @@ public class NERProcess extends LongRunningProcess implements Runnable {
                 final Cell cell = row.getCell(cellIndex);
                 final Serializable cellValue = cell == null ? null : cell.value;
                 final String text = cellValue == null ? "" : cellValue.toString().trim();
+                
                 // Perform extraction if the text is not empty
-                if (text.length() > 0) {
-                    LOGGER.info(String.format("Extracting named entities in column %s on row %d of %d.",
-                                              column.getName(), rowsProcessed + 1, rowsFiltered));
-                    namedEntities[rowIndex] = performExtraction(text);
-                }
+                LOGGER.info(String.format("Extracting named entities in column %s on row %d of %d.",
+      				  column.getName(), rowsProcessed + 1, rowsFiltered));
+                extractionResults[rowIndex] = text.isEmpty() ? new ExtractionResult[0] : performExtraction(text);
+                
                 _progress = 100 * ++rowsProcessed / rowsFiltered;
             }
             // Exit directly if the process has been cancelled
             if (_canceled)
                 return null;
         }
-        return namedEntities;
+        return extractionResults;
     }
 
     
@@ -122,7 +119,7 @@ public class NERProcess extends LongRunningProcess implements Runnable {
      * @param text The text
      * @return The extracted named entities per service
      */
-    protected NamedEntity[][] performExtraction(final String text) {
+    protected ExtractionResult[] performExtraction(final String text) {
         // The execution of the services happens in parallel.
         // Create the extractors and corresponding threads
         final Extractor[] extractors = new Extractor[services.size()];
@@ -134,7 +131,7 @@ public class NERProcess extends LongRunningProcess implements Runnable {
         }
         
         // Wait for all threads to finish and collect their results
-        final NamedEntity[][] extractions = new NamedEntity[extractors.length][];
+        final ExtractionResult[] extractionResults = new ExtractionResult[extractors.length];
         for (i = 0; i < extractors.length; i++) {
             try {
                 extractors[i].join();
@@ -142,9 +139,9 @@ public class NERProcess extends LongRunningProcess implements Runnable {
             catch (InterruptedException error) {
                 LOGGER.error("The extractor was interrupted", error);
             }
-            extractions[i] = extractors[i].getNamedEntities();
+            extractionResults[i] = extractors[i].getExtractionResult();
         }
-        return extractions;
+        return extractionResults;
     }
     
     /**
@@ -183,13 +180,11 @@ public class NERProcess extends LongRunningProcess implements Runnable {
      * Thread that executes a named-entity recognition service
      */
     protected static class Extractor extends Thread {
-        private final static NamedEntity[] EMPTY_ENTITY_SET = new NamedEntity[0];
-        
         private final String text;
         private final NERService service;
         private final Map<String,String> settings;
-        private NamedEntity[] namedEntities;
-        
+        private ExtractionResult extractionResult;
+
         /**
          * Creates a new <tt>Extractor</tt>
          * @param text The text to analyze
@@ -200,25 +195,24 @@ public class NERProcess extends LongRunningProcess implements Runnable {
             this.text = text;
             this.service = service;
             this.settings = settings;
-            this.namedEntities = EMPTY_ENTITY_SET;
         }
         
         /**
-         * Gets the named entities the service extracted from the text
+         * Gets the result of the named-entity recognition service
          * @return The extracted named entities
          */
-        public NamedEntity[] getNamedEntities() {
-            return namedEntities;
+        public ExtractionResult getExtractionResult() {
+            return extractionResult;
         }
         
         /** {@inheritDoc} */
         @Override
         public void run() {
             try {
-                namedEntities = service.extractNamedEntities(text, settings);
+                extractionResult = new ExtractionResult(service.extractNamedEntities(text, settings));
             }
             catch (Exception error) {
-                LOGGER.error("The extractor failed", error);
+                extractionResult = new ExtractionResult(error);
             }
         }
     }

src/org/freeyourmetadata/ner/operations/NERChange.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.lang.ArrayUtils;
+import org.freeyourmetadata.ner.services.ExtractionResult;
 import org.freeyourmetadata.ner.services.NamedEntity;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,6 +19,7 @@ import org.json.JSONTokener;
 import org.json.JSONWriter;
 
 import com.google.refine.history.Change;
+import com.google.refine.model.Cell;
 import com.google.refine.model.Project;
 import com.google.refine.model.Row;
 import com.google.refine.model.changes.CellAtRow;
@@ -33,19 +35,20 @@ import com.google.refine.util.Pool;
 public class NERChange implements Change {
     private final int columnIndex;
     private final String[] serviceNames;
-    private final NamedEntity[][][] namedEntities;
+    private final ExtractionResult[][] extractionResults;
     private final List<Integer> addedRowIds;
     
     /**
      * Creates a new <tt>NERChange</tt>
      * @param columnIndex The index of the column used for named-entity recognition
      * @param serviceNames The names of the used services
-     * @param namedEntities The extracted named entities per row and service
+     * @param extractionResults The results of named-entity extraction per row and service
      */
-    public NERChange(final int columnIndex, final String[] serviceNames, final NamedEntity[][][] namedEntities) {
+    public NERChange(final int columnIndex, final String[] serviceNames,
+    				 final ExtractionResult[][] extractionResults) {
         this.columnIndex = columnIndex;
         this.serviceNames = serviceNames;
-        this.namedEntities = namedEntities;
+        this.extractionResults = extractionResults;
         this.addedRowIds = new ArrayList<Integer>();
     }
 
@@ -79,19 +82,28 @@ public class NERChange implements Change {
             json.key("column"); json.value(columnIndex);
             json.key("services"); JSONUtilities.writeStringArray(json, serviceNames);
             json.key("entities");
-            /* Named entities nested array */
+            /* Nested array of extraction results */
             {
-                /* Rows array */
+            	/* Array of results per row */
                 json.array();
-                for (final NamedEntity[][] row : namedEntities) {
-                    /* Services array */
+                for (final ExtractionResult[] rowResults : extractionResults) {
+                	/* Array of results per service on this row */
                     json.array();
-                    /* Service results array */
-                    for (final NamedEntity[] entities : row) {
-                        json.array();
-                        for (final NamedEntity entity : entities)
-                            entity.writeTo(json);
-                        json.endArray();
+                    /* Array of results for this service */
+                    for (final ExtractionResult extractionResult : rowResults) {
+                    	/* Array of entities */
+                    	if (!extractionResult.hasError()) {
+	                        json.array();
+	                        for (final NamedEntity entity : extractionResult.getNamedEntities())
+	                            entity.writeTo(json);
+	                        json.endArray();
+                    	}
+                    	/* Error object */
+                    	else {
+                    		json.object();
+                    		json.key("error"); json.value(extractionResult.getExtractionError().message);
+                    		json.endObject();
+                    	}
                     }
                     json.endArray();
                 }
@@ -128,31 +140,36 @@ public class NERChange implements Change {
         final int columnIndex = changeJson.getInt("column");
         final String[] serviceNames = JSONUtilities.getStringArray(changeJson, "services");
         
-        /* Named entities nested array */
+        /* Nested array of extraction results */
         final JSONArray namedEntitiesJson = changeJson.getJSONArray("entities");
-        final NamedEntity[][][] namedEntities = new NamedEntity[namedEntitiesJson.length()][][];
-        /* Rows array */
-        for (int i = 0; i < namedEntities.length; i++) {
-            /* Services array */
-            final JSONArray serviceResultsJson = namedEntitiesJson.getJSONArray(i);
-            final NamedEntity[][] serviceResults = namedEntities[i] = new NamedEntity[serviceResultsJson.length()][];
-            for (int j = 0; j < serviceResults.length; j++) {
-                /* Service results array */
-                final JSONArray entitiesJson = serviceResultsJson.getJSONArray(j);
-                final NamedEntity[] entities = serviceResults[j] = new NamedEntity[serviceResultsJson.length()];
-                for (int k = 0; k < entities.length; k++) {
-                    try {
-                        entities[k] = new NamedEntity(entitiesJson.getJSONObject(k));
-                    }
-                    catch (JSONException e) {
-                        entities[k] = new NamedEntity("");
-                    }
-                }
+        final ExtractionResult[][] extractionResults = new ExtractionResult[namedEntitiesJson.length()][];
+        /* Array of results per row */
+        for (int i = 0; i < extractionResults.length; i++) {
+            /* Array of results per service on this row */
+            final JSONArray rowResultsJson = namedEntitiesJson.getJSONArray(i);
+            final ExtractionResult[] rowResults = new ExtractionResult[rowResultsJson.length()];
+            for (int j = 0; j < rowResults.length; j++) {
+            	final JSONObject error = rowResultsJson.optJSONObject(j);
+            	if (error == null) {
+            		/* Array of entities */
+            		final JSONArray entitiesJson = rowResultsJson.getJSONArray(j);
+	                final NamedEntity[] entities = new NamedEntity[rowResultsJson.length()];
+	                for (int k = 0; k < entities.length; k++) {
+	                    try { entities[k] = new NamedEntity(entitiesJson.getJSONObject(k)); }
+	                    catch (JSONException e) { entities[k] = new NamedEntity(""); }
+	                }
+	                rowResults[j] = new ExtractionResult(entities);
+            	}
+		        else {
+		        	/* Error object */
+		        	rowResults[j] = new ExtractionResult(new Exception(error.getString("error")));
+		        }
             }
+            extractionResults[i] = rowResults;
         }
         
         /* Reconstruct change object */
-        final NERChange change = new NERChange(columnIndex, serviceNames, namedEntities);
+        final NERChange change = new NERChange(columnIndex, serviceNames, extractionResults);
         for (final int addedRowId : JSONUtilities.getIntArray(changeJson, "addedRows"))
             change.addedRowIds.add(addedRowId);
         return change;
@@ -212,33 +229,40 @@ public class NERChange implements Change {
         // Add the extracted named entities to all rows, creating new ones as necessary
         int rowNumber = 0;
         addedRowIds.clear();
-        for (final NamedEntity[][] serviceEntities : namedEntities) {
+        for (final ExtractionResult[] rowResults : extractionResults) {
             // Determine the maximum number of named entities per service
             int maxEntities = 0;
-            for (final NamedEntity[] entities : serviceEntities)
-                maxEntities = Math.max(maxEntities, entities.length);
-            // Skip this row if no named entities were found
-            if (maxEntities == 0) {
-                rowNumber++;
-                continue;
+            for (int col = 0; col < rowResults.length; col++) {
+            	int neededCells = rowResults[col].hasError() ? 1 : rowResults[col].getNamedEntities().length;
+                maxEntities = Math.max(maxEntities, neededCells);
             }
-            // Create new blank rows if named entities don't fit on a single line
-            for (int i = 1; i < maxEntities; i++) {
-                final Row entityRow = new Row(minRowSize);
-                final int entityRowId = rowNumber + i;
-                for (int j = 0; j < minRowSize; j++)
-                    entityRow.cells.add(null);
-                rows.add(entityRowId, entityRow);
-                addedRowIds.add(entityRowId);
-            }
-            // Place all named entities
-            for (int c = 0; c < serviceEntities.length; c++) {
-                final NamedEntity[] entities = serviceEntities[c];
-                for (int r = 0; r < entities.length; r++)
-                    rows.get(rowNumber + r).cells.set(cellIndexes[c], entities[r].toCell());
+            if (maxEntities > 0) {
+	            // Create new blank rows if the results don't fit on a single line
+	            for (int i = 1; i < maxEntities; i++) {
+	                final Row entityRow = new Row(minRowSize);
+	                final int entityRowId = rowNumber + i;
+	                for (int j = 0; j < minRowSize; j++)
+	                    entityRow.cells.add(null);
+	                rows.add(entityRowId, entityRow);
+	                addedRowIds.add(entityRowId);
+	            }
+	            // Place all results
+	            for (int col = 0; col < rowResults.length; col++) {
+	            	// Place each found entity on a row
+	            	if (!rowResults[col].hasError()) {
+		                final NamedEntity[] entities = rowResults[col].getNamedEntities();
+		                for (int r = 0; r < entities.length; r++)
+		                    rows.get(rowNumber + r).cells.set(cellIndexes[col], entities[r].toCell());
+	            	}
+	            	// Place an error only on the first row
+	            	else {
+	            		final Cell errorCell = new Cell(rowResults[col].getExtractionError(), null);
+	            		rows.get(rowNumber).cells.set(cellIndexes[col], errorCell);
+	            	}
+	            }
             }
             // Advance to the next original row
-            rowNumber += maxEntities;
+            rowNumber += Math.max(1, maxEntities);
         }
     }
     

src/org/freeyourmetadata/ner/services/DataTXT.java
@@ -17,6 +17,7 @@ import org.json.JSONTokener;
 /**
  * dataTXT service connector
  * @author Stefano Parmesan
+ * @author Giuliano Tortoreto
  */
 public class DataTXT extends NERServiceBase {
     private final static URI SERVICEBASEURL = createUri("https://api.dandelion.eu/datatxt/nex/v1");
@@ -77,5 +78,12 @@ public class DataTXT extends NERServiceBase {
         }
         
         return results;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected Exception extractError(final JSONTokener tokener) throws JSONException {
+    	final JSONObject response = (JSONObject)tokener.nextValue();
+    	return response.has("message") ? new Exception(response.getString("message")) : null;
     }
 }

src/org/freeyourmetadata/ner/services/ExtractionResult.java
@@ -1,0 +1,55 @@
+package org.freeyourmetadata.ner.services;
+
+import com.google.refine.expr.EvalError;
+
+/**
+ * A result of a single named-entity extraction service call.
+ * @author Giuliano Tortoreto
+ * @author Ruben Verborgh
+ */
+public class ExtractionResult {
+    private final NamedEntity[] namedEntities;
+    private final EvalError extractionError;
+
+    /**
+     * Creates a successful named-entity extraction result
+     * @param namedEntities The list of extracted named entities
+     */
+    public ExtractionResult(final NamedEntity[] namedEntities) {
+        this.namedEntities = namedEntities;
+        this.extractionError = null;
+    }
+    
+    /**
+     * Creates an unsuccessful named-entity extraction result
+     * @param extractionError The error that occurred during extraction
+     */
+    public ExtractionResult(final Throwable extractionError) {
+        this.namedEntities = null;
+        this.extractionError = new EvalError(extractionError.getMessage());
+    }
+
+    /**
+     * Gets the extracted named entities
+     * @return The extracted named entities
+     */
+    public NamedEntity[] getNamedEntities() {
+        return namedEntities == null ? new NamedEntity[0] : namedEntities;
+    }
+    
+    /**
+     * Gets the error that occurred during the extraction
+     * @return The extracted named entities
+     */
+    public EvalError getExtractionError() {
+        return extractionError;
+    }
+
+    /**
+     * Indicates whether an error occurred during extraction.
+     * @return <tt>true</tt> if an error occurred
+     */
+    public boolean hasError() {
+        return extractionError != null;
+    }
+}

src/org/freeyourmetadata/ner/services/NERServiceBase.java
@@ -15,7 +15,6 @@ import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.InputStreamEntity;
@@ -130,11 +129,9 @@ public abstract class NERServiceBase implements NERService {
      */
     protected NamedEntity[] performExtractionRequest(final HttpUriRequest request) throws Exception {
         final DefaultHttpClient httpClient = new DefaultHttpClient();
-        final HttpResponse response = httpClient.execute(request);
-        if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK)
-            throw new IllegalStateException(
-                    String.format("The extraction request returned status code %d instead of %s.",
-                                  response.getStatusLine().getStatusCode(), HttpStatus.SC_OK));
+        final Exception error = extractError(response);
+        if (error != null) throw error;
+        
         final HttpEntity responseEntity = response.getEntity();
         return parseExtractionResponseEntity(responseEntity);
     }
@@ -232,5 +229,29 @@ public abstract class NERServiceBase implements NERService {
         catch (UnsupportedEncodingException error) {
             throw new RuntimeException(error);
         }
+    }
+    
+    /**
+     * Extracts a possible error from the HTTP response
+     * @param response The response
+     * @return The extracted error, or <tt>null</tt> if none exists
+     */
+    protected Exception extractError(final HttpResponse response) throws Exception {
+        if (response.getStatusLine().getStatusCode() < 300) return null;
+        try {
+            return extractError(new JSONTokener(new InputStreamReader(response.getEntity().getContent())));
+        }
+        catch (Exception error) {
+        	return new Exception(String.format("HTTP error %d", response.getStatusLine().getStatusCode()));
+        }
+    }
+    
+    /**
+     * Extracts a possible error from the HTTP response body
+     * @param tokener The tokener containing the response
+     * @return The extracted error, or <tt>null</tt> if none exists
+     */
+    protected Exception extractError(final JSONTokener tokener) throws JSONException {
+    	throw new UnsupportedOperationException();
     }
 }

commit 63bf98f6370c925cb964b484d0616b76d8bee26a

Give empty result for non-filtered rows.
A bug was introduced in 09def59d005d817fc84bbdf8319f1647064033af,
leaving null as extracted result for non-filtered rows.

src/org/freeyourmetadata/ner/operations/NERProcess.java
@@ -89,6 +89,7 @@ public class NERProcess extends LongRunningProcess implements Runnable {
         
         // Go through each row and extract entities if the row is part of the filter
         final ExtractionResult[][] extractionResults = new ExtractionResult[rowsTotal][];
+        final ExtractionResult[] emptyResult = new ExtractionResult[0];
         int rowsProcessed = 0;
         for (int rowIndex = 0; rowIndex < rowsTotal; rowIndex++) {
             // If the row is part of the filter, extract entities
@@ -102,9 +103,12 @@ public class NERProcess extends LongRunningProcess implements Runnable {
                 // Perform extraction if the text is not empty
                 LOGGER.info(String.format("Extracting named entities in column %s on row %d of %d.",
       				  column.getName(), rowsProcessed + 1, rowsFiltered));
-                extractionResults[rowIndex] = text.isEmpty() ? new ExtractionResult[0] : performExtraction(text);
+                extractionResults[rowIndex] = text.isEmpty() ? emptyResult : performExtraction(text);
                 
                 _progress = 100 * ++rowsProcessed / rowsFiltered;
+            }
+            else {
+            	extractionResults[rowIndex] = emptyResult;
             }
             // Exit directly if the process has been cancelled
             if (_canceled)